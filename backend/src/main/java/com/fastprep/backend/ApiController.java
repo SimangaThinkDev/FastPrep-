29e90043a7756da938421f3de78e64a4ec0a01b2
Sean Kembo <sekembojhb024@student.wethinkcode.co.za>
2025-11-16 17:30:18 +0200

    endpoint to retrieve json data

---
+++ backend/src/main/java/com/fastprep/backend/ApiController.java
@@ -0,0 +1,39 @@
+package com.fastprep.backend;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+
+@RestController
+@RequestMapping("api/exams")
+public class ApiController {
+
+
+    private static final String BASE_PATH = "data_scraping/exams_data_as_json/";
+
+
+    @GetMapping("/{id}")
+    public Object getExam(@PathVariable int id) throws IOException {
+
+        try {
+            String filename = "practice-exam-" + id + ".json";
+            Path path = Path.of("data_scraping/exams_data_as_json/" + filename);
+
+            String json = Files.readString(path);
+
+            ObjectMapper mapper = new ObjectMapper();
+            return mapper.readValue(json, Object.class);
+        } catch (Exception e) {
+            return Map.of("error", "Exam not found");
+        }
+
+    }
+}
