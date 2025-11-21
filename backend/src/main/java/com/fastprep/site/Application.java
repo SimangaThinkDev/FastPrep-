fbc4301ca0e6e01f6a52337427f8eabe950c1127
Simangaliso Phakwe <siphakwjhb024@student.wethinkcode.co.za>
2025-11-21 14:13:57 +0200

    Adding logger instead and removing print statements...

--- backend/src/main/java/com/fastprep/site/Application.java
+++ backend/src/main/java/com/fastprep/site/Application.java
@@ -1,24 +1,36 @@
 package com.fastprep.site;
 
+import org.apache.logging.slf4j.SLF4JLogger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import static com.fastprep.site.utils.Ansi.*;
 
 @SpringBootApplication
-public class Application {
+public class Application implements ApplicationRunner {
 
     @Value("${spring.application.server_url}")
-    private static String serverUrl;
+    private String serverUrl;
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+    private static final Logger logger = LoggerFactory.getLogger( Application.class );
 
-        // Easy access to homepage ! Remove for production environments
-        System.out.println(
-                green( "Live on " + hyperlink( serverUrl, "" ) )
-        );
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    /**
+     * This serves a simple a straightforward purpose.
+     * Show where the server is currently running.
+     * Also works in production...
+     */
+    @Override
+    public void run(ApplicationArguments args) {
+        logger.info( "\nApplication running on " + serverUrl );
+    }
 
 }
